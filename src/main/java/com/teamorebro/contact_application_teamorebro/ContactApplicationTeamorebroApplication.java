fd33c5694d54587d5e9a5724b8f931af613798f3
AliciaGrevsten <alicia.grevsten@gmail.com>
2020-08-06 14:55:08 +0200

    Initial commit

---
+++ src/main/java/com/teamorebro/contact_application_teamorebro/ContactApplicationTeamorebroApplication.java
@@ -0,0 +1,13 @@
+package com.teamorebro.contact_application_teamorebro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactApplicationTeamorebroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactApplicationTeamorebroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamorebro/contact_application_teamorebro/ContactApplicationTeamorebroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamorebro.contact_application_teamorebro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactApplicationTeamorebroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
