15430dbe577ad9a48f1b0cc286abf372df56cfeb
AliciaGrevsten <alicia.grevsten@gmail.com>
2020-08-06 16:05:57 +0200

    Test

--- src/main/java/com/teamorebro/contact_application_teamorebro/controllers/ContactController.java
+++ src/main/java/com/teamorebro/contact_application_teamorebro/controllers/ContactController.java
@@ -1,13 +1,21 @@
 package com.teamorebro.contact_application_teamorebro.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ContactController {
 
     @GetMapping("/")
     public String getContacts() {
+
+        return "";
+    }
+
+    @GetMapping("/search")
+    public String searchContact(@RequestParam(name="input", required = false, defaultValue = "") String searchInput, Model model) {
 
         return "";
     }
